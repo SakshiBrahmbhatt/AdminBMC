6a5852507612f62b5ed2b0e1fbfe695e78a37439
Vanshika <vanshikag0111@gmail.com>
2024-02-19 15:12:30 +0530

    connection to mqtt and displayed data from mysql

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            serverData = new DataGridView();
+            subscribeTopic = new DataGridView();
+            statLbl = new Label();
+            filterMyData = new CheckBox();
+            searchText = new TextBox();
+            searchBtn = new Button();
+            users = new DataGridView();
+            addUser = new Button();
+            topicBox = new TextBox();
+            Connbtn = new Button();
+            subBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)serverData).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)subscribeTopic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)users).BeginInit();
+            SuspendLayout();
+            // 
+            // serverData
+            // 
+            serverData.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            serverData.BackgroundColor = SystemColors.InactiveCaption;
+            serverData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            serverData.Location = new Point(12, 272);
+            serverData.Name = "serverData";
+            serverData.RowHeadersWidth = 51;
+            serverData.Size = new Size(1768, 270);
+            serverData.TabIndex = 5;
+            // 
+            // subscribeTopic
+            // 
+            subscribeTopic.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            subscribeTopic.BackgroundColor = SystemColors.InactiveCaption;
+            subscribeTopic.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            subscribeTopic.Location = new Point(361, 12);
+            subscribeTopic.Name = "subscribeTopic";
+            subscribeTopic.RowHeadersWidth = 51;
+            subscribeTopic.Size = new Size(455, 187);
+            subscribeTopic.TabIndex = 8;
+            subscribeTopic.CellContentClick += subscribeTopic_CellContentClick;
+            // 
+            // statLbl
+            // 
+            statLbl.AutoSize = true;
+            statLbl.Location = new Point(122, 16);
+            statLbl.Name = "statLbl";
+            statLbl.Size = new Size(172, 20);
+            statLbl.TabIndex = 9;
+            statLbl.Text = "Not connected to broker";
+            // 
+            // filterMyData
+            // 
+            filterMyData.AutoSize = true;
+            filterMyData.Location = new Point(12, 237);
+            filterMyData.Name = "filterMyData";
+            filterMyData.Size = new Size(157, 24);
+            filterMyData.TabIndex = 10;
+            filterMyData.Text = "Only show my data";
+            filterMyData.UseVisualStyleBackColor = true;
+            // 
+            // searchText
+            // 
+            searchText.Location = new Point(214, 238);
+            searchText.Name = "searchText";
+            searchText.PlaceholderText = "topic/#";
+            searchText.Size = new Size(166, 27);
+            searchText.TabIndex = 11;
+            // 
+            // searchBtn
+            // 
+            searchBtn.Location = new Point(386, 237);
+            searchBtn.Name = "searchBtn";
+            searchBtn.Size = new Size(94, 29);
+            searchBtn.TabIndex = 12;
+            searchBtn.Text = "Search";
+            searchBtn.UseVisualStyleBackColor = true;
+            // 
+            // users
+            // 
+            users.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            users.BackgroundColor = SystemColors.InactiveCaption;
+            users.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            users.Location = new Point(869, 12);
+            users.Name = "users";
+            users.RowHeadersWidth = 51;
+            users.Size = new Size(455, 187);
+            users.TabIndex = 13;
+            // 
+            // addUser
+            // 
+            addUser.Location = new Point(869, 205);
+            addUser.Name = "addUser";
+            addUser.Size = new Size(138, 29);
+            addUser.TabIndex = 14;
+            addUser.Text = "Add New User";
+            addUser.UseVisualStyleBackColor = true;
+            // 
+            // topicBox
+            // 
+            topicBox.Location = new Point(122, 61);
+            topicBox.Name = "topicBox";
+            topicBox.PlaceholderText = "topic/#";
+            topicBox.Size = new Size(166, 27);
+            topicBox.TabIndex = 15;
+            // 
+            // Connbtn
+            // 
+            Connbtn.Location = new Point(12, 12);
+            Connbtn.Name = "Connbtn";
+            Connbtn.Size = new Size(94, 29);
+            Connbtn.TabIndex = 16;
+            Connbtn.Text = "Connect";
+            Connbtn.UseVisualStyleBackColor = true;
+            Connbtn.Click += Connbtn_Click;
+            // 
+            // subBtn
+            // 
+            subBtn.Location = new Point(12, 59);
+            subBtn.Name = "subBtn";
+            subBtn.Size = new Size(94, 29);
+            subBtn.TabIndex = 17;
+            subBtn.Text = "Subscribe";
+            subBtn.UseVisualStyleBackColor = true;
+            subBtn.Click += subBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1792, 554);
+            Controls.Add(subBtn);
+            Controls.Add(Connbtn);
+            Controls.Add(topicBox);
+            Controls.Add(addUser);
+            Controls.Add(users);
+            Controls.Add(searchBtn);
+            Controls.Add(searchText);
+            Controls.Add(filterMyData);
+            Controls.Add(statLbl);
+            Controls.Add(subscribeTopic);
+            Controls.Add(serverData);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)serverData).EndInit();
+            ((System.ComponentModel.ISupportInitialize)subscribeTopic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)users).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView serverData;
+        private DataGridView subscribeTopic;
+        private Label statLbl;
+        private CheckBox filterMyData;
+        private TextBox searchText;
+        private Button searchBtn;
+        private DataGridView users;
+        private Button addUser;
+        private TextBox topicBox;
+        private Button Connbtn;
+        private Button subBtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,250 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using MySql.Data.MySqlClient;
+using System.Data;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
 namespace AdminBMC
 {
+
     public partial class Form1 : Form
     {
+        //mqtt connection
+        bool connect = false;
+        private MqttClient mqttClient;
+        private List<string> subscribedTopics = new List<string>();
+        //private List<MessageData> messages = new List<MessageData>();
+
+        // MySQL connection
+        MySqlConnection con = new MySqlConnection("SERVER = 192.168.1.9; DATABASE = sys; UID = db; PASSWORD = Saks@2468;");
+
+        // DeviceId field
+        private string deviceId;
+        private string username;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Connbtn_Click(object sender, EventArgs e)
+        {
+            if (connect == false)
+            {
+                try
+                {
+                    mqttClient = new MqttClient("www.mqtt-dashboard.com", 1883, false, null, null, MqttSslProtocols.None);
+                    mqttClient.Connect(Guid.NewGuid().ToString());
+                    MessageBox.Show("Connected to MQTT broker!");
+                    Connbtn.Text = "Disconnect";
+                    statLbl.Text = "Connected to broker";
+                    connect = true;
+                    try
+                    {
+                        con.Open();
+                        LoadSubscribedTopics();
+                        LoadUsersData();
+                        con.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Cannot open connection!");
+                    }
+                    MySqlDataAdapter da = new MySqlDataAdapter("select * from subscribe_topic", con);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    subscribeTopic.DataSource = ds.Tables[0];
+                    mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+                    viewDataTable();
+                }
+                catch (MqttConnectionException ex)
+                {
+                    MessageBox.Show("Error connecting to MQTT broker: " + ex.Message);
+                }
+            }
+            else
+            {
+                mqttClient.Disconnect();
+                Connbtn.Text = "Connect";
+                statLbl.Text = "Not Connected to broker";
+                connect = false;
+            }
+        }
+
+        private void subBtn_Click(object sender, EventArgs e)
+        {
+            if (connect)
+            {
+                if (!string.IsNullOrEmpty(topicBox.Text))
+                {
+                    string topic = topicBox.Text.Trim();
+
+                    if (!string.IsNullOrEmpty(topic) && !subscribedTopics.Contains(topic))
+                    {
+                        mqttClient.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+                        subscribedTopics.Add(topic);
+                        try
+                        {
+                            con.Open();
+                            string insertQuery = "INSERT INTO subscribe_topic (Topic, Status) VALUES (@topic, @status)";
+                            MySqlCommand cmd = new MySqlCommand(insertQuery, con);
+                            cmd.Parameters.AddWithValue("@Topic", topic);
+                            cmd.Parameters.AddWithValue("@Status", 1);
+                            cmd.ExecuteNonQuery();
+
+                            MessageBox.Show("Topic inserted into subscribe_topic table!");
+
+                            MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM subscribe_topic", con);
+                            DataTable dt = new DataTable();
+                            da.Fill(dt);
+                            subscribeTopic.DataSource = dt;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error inserting into subscribe_topic table: " + ex.Message);
+                        }
+                        finally
+                        {
+                            con.Close();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Enter a topic to subscribe");
+                }
+            }
+            else
+            {
+                MessageBox.Show("First connect to the broker");
+            }
+        }
+
+        private void subscribeTopic_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void LoadSubscribedTopics()
+        {
+            try
+            {
+                subscribedTopics.Clear();
+
+                string query = "SELECT Topic FROM subscribe_topic";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string topic = reader["Topic"].ToString();
+                        subscribedTopics.Add(topic);
+
+                        // Subscribe to the topic
+                        mqttClient.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading subscribed topics: " + ex.Message);
+            }
+        }
+
+        private void LoadUsersData()
+        {
+            try
+            {
+                string query = "SELECT * FROM users";
+                MySqlDataAdapter cmd = new MySqlDataAdapter(query, con);
+                
+                DataTable dt = new DataTable();
+                cmd.Fill(dt);
+                users.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading user data: " + ex.Message);
+            }
+        }
+
+        private void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            if (connect)
+            {
+                string messageText = System.Text.Encoding.UTF8.GetString(e.Message);
+                string topic = e.Topic;
+                List<string> parts = new List<string>(messageText.Split(','));
+                BeginInvoke((Action)(() =>
+                {
+                    try
+                    {
+                        con.Open();
+                        string insertQuery = "INSERT INTO bmcmqtt (DeviceId,BMCODE,Temperature,Pressure,Volume,Level,Generator,Grid,Aggregate,Compressor1,compressor2,VoltageU,VoltageV,VoltageW,CurrentU,CurrentV,CurrentW,Frequency,PwrF,TPwr,Time,Date,Topic) VALUES (@DeviceId,@BMCODE,@Temp,@Press,@Vol,@Lvl,@Gen,@Grid,@Agr,@Comp1,@Comp2,@VoltU,@VoltV,@VoltW,@CurrU,@CurrV,@CurrW,@Freq,@PwrF,@TPwr,@Time,@Date,@Topic)";
+                        MySqlCommand cmd = new MySqlCommand(insertQuery, con);
+                        cmd.Parameters.AddWithValue("@DeviceId", deviceId);
+                        cmd.Parameters.AddWithValue("@BMCODE", parts[0]);
+                        cmd.Parameters.AddWithValue("@Temp", parts[1]);
+                        cmd.Parameters.AddWithValue("@Press", parts[2]);
+                        cmd.Parameters.AddWithValue("@Vol", parts[3]);
+                        cmd.Parameters.AddWithValue("@Lvl", parts[4]);
+                        cmd.Parameters.AddWithValue("@Gen", parts[5]);
+                        cmd.Parameters.AddWithValue("@Grid", parts[6]);
+                        cmd.Parameters.AddWithValue("@Agr", parts[7]);
+                        cmd.Parameters.AddWithValue("@Comp1", parts[8]);
+                        cmd.Parameters.AddWithValue("@Comp2", parts[9]);
+                        cmd.Parameters.AddWithValue("@VoltU", parts[10]);
+                        cmd.Parameters.AddWithValue("@VoltV", parts[11]);
+                        cmd.Parameters.AddWithValue("@VoltW", parts[12]);
+                        cmd.Parameters.AddWithValue("@CurrU", parts[13]);
+                        cmd.Parameters.AddWithValue("@CurrV", parts[14]);
+                        cmd.Parameters.AddWithValue("@CurrW", parts[15]);
+                        cmd.Parameters.AddWithValue("@Freq", parts[16]);
+                        cmd.Parameters.AddWithValue("@Pwrf", parts[17]);
+                        cmd.Parameters.AddWithValue("@TPwr", parts[18]);
+                        cmd.Parameters.AddWithValue("@Time", parts[19]);
+                        cmd.Parameters.AddWithValue("@Date", parts[20].Substring(0, 12));
+                        cmd.Parameters.AddWithValue("@Topic", topic);
+                        cmd.ExecuteNonQuery();
+
+                        // Retrieve data from the database and update the DataGridView
+                        MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM bmcmqtt", con);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        serverData.DataSource = dt;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error inserting into bmcmqtt table: {ex.Message}");
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+                }));
+            }
+        }
+
+        void viewDataTable()
+        {
+            try
+            {
+                con.Open();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection!");
+            }
+            MySqlDataAdapter da = new MySqlDataAdapter("select * from bmcmqtt", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            serverData.DataSource = ds.Tables[0];
         }
     }
 }
